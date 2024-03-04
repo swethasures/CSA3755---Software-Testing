42f8b1692c5a2bad1f33fead9963f6132015c02b
192121009 Swetha <112751881+swethasures@users.noreply.github.com>
2024-03-04 18:31:52 +0530

    Add files via upload

---
+++ Reverse_word.java
@@ -0,0 +1,11 @@
+public class Reverse_word{
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String inputString = "Hello, World!";
+        String reversedStr = reverseString(inputString);
+        System.out.println("Reversed String: " + reversedStr);
+    }
+}
